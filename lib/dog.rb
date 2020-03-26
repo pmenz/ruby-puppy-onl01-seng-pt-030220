196af7bdba2736cb7189ef8f1a66460890c88d34
Pablo Menz <p.menz.r@gmail.com>
2020-03-26 23:50:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,22 @@
   class Dog 
  
   @@all = []
+<<<<<<< HEAD
                                         #I gave up, I couldn't get to pull the names from @@all
   @@puppies=[]                          #  I tried each and other methos but the name were
                                         # not save into @@all" 
+=======
+  @@new_puppie=[]
+>>>>>>> 424c155b6395ca9cfbfbe4c21566dffa47b66a06
   attr_accessor :name
   
   def initialize(name)
   @name = name
   save
+<<<<<<< HEAD
   @@puppies<< name
+=======
+>>>>>>> 424c155b6395ca9cfbfbe4c21566dffa47b66a06
   end
 
   def save
@@ -26,7 +33,11 @@
   end
  
   def self.print_all 
+<<<<<<< HEAD
   puts @@puppies.uniq-["Snoopy"]
+=======
+  puts @@new_puppie
+>>>>>>> 424c155b6395ca9cfbfbe4c21566dffa47b66a06
   end
 
   end
